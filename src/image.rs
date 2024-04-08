0254c9dae61bc46f8f4e0845a3c8b28fc67d5b0e
Muhammad Mahdi Norouzi <muhammadmnorouzi@gmail.com>
2024-04-08 17:19:02 +0300

    draw triangle implemented

--- src/image.rs
+++ src/image.rs
@@ -102,6 +102,75 @@ impl Image {
         }
     }
 
+    pub fn draw_triangle(
+        &mut self, 
+        x1: usize, 
+        y1: usize, 
+        x2: usize, 
+        y2: usize, 
+        x3: usize, 
+        y3: usize) {
+
+        let (mut x1, mut y1, mut x2, mut y2, mut x3, mut y3) = (x1 as f64, y1 as f64, x2 as f64, y2 as f64, x3 as f64, y3 as f64);
+        Image::order_triangle_vertices_by_y(&mut x1, &mut y1, &mut x2, &mut y2, &mut x3, &mut y3);
+
+        let dx12 = x2 - x1;
+        let dy12 = y2 - y1;
+        let dx13 = x3 - x1;
+        let dy13 = y3 - y1;
+
+        for y in y1 as i64..=y2 as i64 {
+            if y >= 0 && y < self.height as i64 {
+                let mut s1 = if dy12 != 0f64 {
+                    (y as f64 - y1) * dx12 / dy12 + x1
+                } else {
+                    x1
+                };
+                let mut s2 = if dy13 != 0f64 {
+                    (y as f64 - y1) * dx13 / dy13 + x1
+                } else {
+                    x1
+                };
+                if s1 > s2 {
+                    std::mem::swap(&mut s1, &mut s2)
+                }
+                for x in s1 as i64..=s2 as i64 {
+                    if x >= 0 && x < self.width as i64 {
+                        self.data[y as usize * self.width + x as usize] = self.color;
+                    }
+                }
+            }
+        }
+
+        let dx32 = x2 - x3;
+        let dy32 = y2 - y3;
+        let dx31 = x1 - x3;
+        let dy31 = y1 - y3;
+
+        for y in y2 as i64..=y3 as i64 {
+            if y >= 0 && y < self.height as i64 {
+                let mut s1 = if dy32 != 0f64 {
+                    (y as f64 - y3) * dx32 / dy32 + x3
+                } else {
+                    x3
+                };
+                let mut s2 = if dy31 != 0f64 {
+                    (y as f64 - y3) * dx31 / dy31 + x3
+                } else {
+                    x3
+                };
+                if s1 > s2 {
+                    std::mem::swap(&mut s1, &mut s2)
+                }
+                for x in s1 as i64..=s2 as i64 {
+                    if x >= 0 && x < self.width as i64 {
+                        self.data[y as usize * self.width + x as usize] = self.color;
+                    }
+                }
+            }
+        }
+    }
+
     fn get_circle_rect_area(
         &self,
         center_x: usize,
@@ -134,5 +203,29 @@ impl Image {
 
     fn order_asc(a: usize, b: usize) -> (usize, usize) {
         (usize::min(a, b), usize::max(a, b))
+    }
+
+    fn order_triangle_vertices_by_y(
+        x1: &mut f64,
+        y1: &mut f64,
+        x2: &mut f64,
+        y2: &mut f64,
+        x3: &mut f64,
+        y3: &mut f64,
+    ) {
+        if *y1 > *y2 {
+            std::mem::swap(x1, x2);
+            std::mem::swap(y1, y2);
+        }
+
+        if *y2 > *y3 {
+            std::mem::swap(x2, x3);
+            std::mem::swap(y2, y3);
+        }
+
+        if *y1 > *y2 {
+            std::mem::swap(x1, x2);
+            std::mem::swap(y1, y2);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() -> std::io::Result<()> {
     image.fill_circle(WIDTH / 2, HEIGHT / 2, 200);
     image.set_color(0xF0000F);
     image.draw_line(0, 0, WIDTH, HEIGHT);
+    image.draw_triangle(10, 10, 0, 400, 20, 450);
     image.save("image.ppm")?;
 
     Ok(())
