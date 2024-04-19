f4f09e44472fe5dcd4667a8c42f75a3ca41657ee
Muhammad Mahdi Norouzi <muhammadmnorouzi@gmail.com>
2024-04-19 17:30:28 +0300

    introduced examples section

--- examples/points_cloud.rs
+++ examples/points_cloud.rs
@@ -1,29 +1,10 @@
-#![allow(dead_code)]
+use graphics_rs::{color, graphics::Graphics, math::vec3::Vec3, shapes::point_cloud::PointCloud, simple_canvas::SimpleCanvas, tools::camera::Camera, traits::{canvas::Canvas, canvas_handler::CanvasHandler}};
 
-mod color;
-mod math;
-mod shapes;
-mod tools;
-mod traits;
-
-use graphics_rs::{
-    graphics::Graphics,
-    math::vec3::Vec3,
-    shapes::point_cloud::PointCloud,
-    simple_canvas::SimpleCanvas,
-    tools::camera::Camera,
-    traits::{canvas::Canvas, canvas_handler::CanvasHandler},
-};
-
-// Constants
-const WIDTH: usize = 800;
-const HEIGHT: usize = 800;
-
-struct MyCanvasHandler {
+struct PointsCloudSample {
     point_cloud: PointCloud,
 }
 
-impl<'a> CanvasHandler for MyCanvasHandler {
+impl<'a> CanvasHandler for PointsCloudSample {
     fn update<T: Canvas>(&mut self, canvas: &mut T) {
         canvas.change_color(color::BLACK);
         canvas.fill();
@@ -36,27 +17,6 @@ impl<'a> CanvasHandler for MyCanvasHandler {
 
         return;
     }
-}
-
-fn main() -> Result<(), String> {
-    let antialiasing = false;
-    let antialiasing_resolution = 1;
-    let fill_color = Some(color::BLACK);
-
-    let mut canvas = SimpleCanvas::new(
-        WIDTH as usize,
-        HEIGHT as usize,
-        fill_color,
-        antialiasing,
-        antialiasing_resolution,
-    );
-
-    let mut graphics = Graphics::create(&mut canvas)?;
-    graphics.run(&mut MyCanvasHandler {
-        point_cloud: create_point_cloud(),
-    })?;
-
-    Ok(())
 }
 
 fn create_point_cloud() -> PointCloud {
@@ -85,4 +45,26 @@ fn create_point_cloud() -> PointCloud {
         Vec3::<f64>::new(0f64, 0f64, 0f64),
         size,
     )
+}
+
+const WIDTH: usize = 800;
+const HEIGHT: usize = 800;
+
+fn main() -> Result<(), String> {
+    let antialiasing = false;
+    let antialiasing_resolution = 1;
+    let fill_color = Some(color::BLACK);
+
+    let mut canvas = SimpleCanvas::new(
+        WIDTH as usize,
+        HEIGHT as usize,
+        fill_color,
+        antialiasing,
+        antialiasing_resolution,
+    );
+
+    let mut graphics = Graphics::create(&mut canvas)?;
+    graphics.run(&mut PointsCloudSample {point_cloud: create_point_cloud()})?;
+
+    Ok(())
 }
