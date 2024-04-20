c67d33bb6c3958f0d55a130f663b39dbc5f1c7b8
Muhammad Mahdi Norouzi <muhammadmnorouzi@gmail.com>
2024-04-20 08:18:29 +0300

    added shapes sample

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -13,26 +13,23 @@ impl<'a> CanvasHandler for ShapesHandler {
         canvas.change_color(color::BLACK);
         canvas.fill();
 
+        canvas.change_color(color::BLUE);
+        canvas.draw_shape(&mut Line::new(400, 0, 400, 800));
+        canvas.draw_shape(&mut Line::new(0, 0, canvas.width(), canvas.height()));
+        canvas.draw_shape(&mut Line::new(0, 400, 800, 400));
+        canvas.draw_shape(&mut Line::new(0, canvas.height(), canvas.width(), 0));
+
+        canvas.change_color(color::RED);
+        canvas.draw_shape(&mut Rect::new(300, 200, 200, 100));
+        canvas.draw_shape(&mut Rect::new(200, 300, 100, 200));
+        canvas.draw_shape(&mut Rect::new(300, 500, 200, 100));
+        canvas.draw_shape(&mut Rect::new(500, 300, 100, 200));
+
         canvas.change_color(color::GREEN);
-        canvas.draw_shape(&mut Circle::new(100, 400, 200));
-
-        // canvas.change_color(color::BLUE);
-        // canvas.draw_shape(&mut Line::new(400, 0, 400, 800));
-        // canvas.draw_shape(&mut Line::new(0, 0, canvas.width(), canvas.height()));
-        // canvas.draw_shape(&mut Line::new(0, 400, 800, 400));
-        // canvas.draw_shape(&mut Line::new(0, canvas.height(), canvas.width(), 0));
-
-        // canvas.change_color(color::RED);
-        // canvas.draw_shape(&mut Rect::new(300, 200, 200, 100));
-        // canvas.draw_shape(&mut Rect::new(200, 300, 100, 200));
-        // canvas.draw_shape(&mut Rect::new(300, 500, 200, 100));
-        // canvas.draw_shape(&mut Rect::new(500, 300, 100, 200));
-
-        // canvas.change_color(color::GREEN);
-        // canvas.draw_shape(&mut Circle::new(200, 200, 50));
-        // canvas.draw_shape(&mut Circle::new(200, 600, 50));
-        // canvas.draw_shape(&mut Circle::new(600, 200, 50));
-        // canvas.draw_shape(&mut Circle::new(600, 600, 50));
+        canvas.draw_shape(&mut Circle::new(200, 200, 50));
+        canvas.draw_shape(&mut Circle::new(200, 600, 50));
+        canvas.draw_shape(&mut Circle::new(600, 200, 50));
+        canvas.draw_shape(&mut Circle::new(600, 600, 50));
 
         return;
     }
